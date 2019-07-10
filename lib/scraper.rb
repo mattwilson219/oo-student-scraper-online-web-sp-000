ff2d51211f038fca61c39360391caffdf42484a4
Matthew Wilson <wilsonmatt219@gmail.com>
2019-07-10 18:45:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,28 @@ class Scraper
 end
 
   def self.scrape_profile_page(profile_url)
-
+    html = open(profile_url)
+    page = Nokogiri::HTML(html)
+    social_links = page.css("div.social-icon-container a")
+    social_link_hash = {}
+    page.css(".social-icon-container")
+    social_links.each do |social_link|
+      if social_link.attribute('href').value.include?("twitter")
+        social_link_hash[:twitter] = social_link.attribute('href').value
+      elsif social_link.attribute('href').value.include?("linkedin")
+        social_link_hash[:linkedin] = social_link.attribute('href').value
+        #binding.pry
+      elsif social_link.attribute('href').value.include?("github")
+        social_link_hash[:github] = social_link.attribute('href').value
+      else
+        social_link_hash[:blog] = social_link.attribute('href').value
+      end
+    end
+    social_link_hash[:profile_quote] = page.css("div.vitals-text-container div").text
+    social_link_hash[:bio] = page.css("p").text
+    social_link_hash
+    #binding.pry
+  end
   end
 
 end
